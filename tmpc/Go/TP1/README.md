b2c3050a8bac77cc10e6d10636dff9ea4f5ed239
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-09-18 15:59:47 +0200

    TP1 init

---
+++ tmpc/Go/TP1/README.md
@@ -0,0 +1,10 @@
+# TMPC Go : TP1
+
+## Useful links
+
+* [Sujet du TP ici](http://users.polytech.unice.fr/~eg/TMPC/Tds/Td1/sujet.html)
+
+## 1. Serveur TCP
+
+Fichier [gosimpleserv.go](http://users.polytech.unice.fr/~eg/TMPC/Tds/Td1/sujet.html)
+
---
+++ tmpc/Go/TP1/gosimpleserv.go
@@ -0,0 +1,52 @@
+//
+// gosimpleserv.go  -- A simple server
+//
+//           Author: Erick Gallesio [eg@unice.fr]
+//    Creation date: 17-Oct-2016 14:42 (eg)
+// Last file update: 17-Oct-2016 15:50 (eg)
+//
+// +++
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+// +slide
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Fprintf(conn, "You said => %s", line)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		handleConnection(conn)
+	}
+}
+
+// ---
+// Local Variables:
+// compile-command: "go run *.go"
+// End:
