dca5eb03927f6ecff93a3b0e9ddf70de207b52f6
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-10-09 16:56:59 +0200

    1.2

--- tmpc/Clojure/TP2/exercice1.clj
+++ tmpc/Clojure/TP2/exercice1.clj
@@ -1,12 +1,15 @@
 #!/usr/bin/env clojure
 
-;; Fibonacci function
+(def a (agent 0))
+
 (defn threadJob [n]
-    (Thread/sleep (rand 50))
-    (println "Thread " n))
+    (Thread/sleep (rand-int 50))
+    (send a (fn [i] (println "Thread " i) n)))
 
 (dotimes [i 1000]
     (future (threadJob i)))
+
+(await a)
 
 ;; Exit program
 (System/exit 0)
